3b9d5c0bf2d2f1c621bce9b27d1fd5b4900daedc
praja09 <dipaprajadpp@gmail.com>
2025-03-08 20:04:06 +0700

    Jobsheet 2 Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,30 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/hello', function () { 
+    return 'Hello World'; 
+}); 
+Route::get('/world', function () { 
+    return 'World'; 
+});
+Route::get('/selamat', function () { 
+    return 'Selamat Datang'; 
+});
+Route::get('/about', function () { 
+    return '2341760143_Dipa Praja Pramono'; 
+});
+Route::get('/user/{name}', function ($name) { 
+    return 'Nama saya '.$name; 
+});
+Route::get('/posts/{post}/comments/{comment}', function 
+($postId, $commentId) { 
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId; 
+}); 
+Route::get('/articles/{id}', function($artId) {
+    return 'Halaman Artikel ID Ke-'.$artId;
+});
+
+#Optional Parameter
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
